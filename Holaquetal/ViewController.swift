8eeafe2523fee6f0b23821bdc5002c0c4b2d023a
jimmy <wjdtjq6@gmail.com>
2024-12-31 00:27:15 +0900

    Initial Commit

---
+++ Holaquetal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Holaquetal
+//
+//  Created by 소정섭 on 12/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
