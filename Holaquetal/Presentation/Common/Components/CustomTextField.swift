bc950fa1f73786dfe8794d845d960f70fc8fabd1
jimmy <wjdtjq6@gmail.com>
2025-01-14 21:39:03 +0900

    UI-CustomTextField Rect수정

--- Holaquetal/Presentation/Common/Components/CustomTextField.swift
+++ Holaquetal/Presentation/Common/Components/CustomTextField.swift
@@ -9,11 +9,12 @@ import UIKit
 
 final class CustomTextField: UITextField {
     
-    let width = UIScreen.main.bounds.width - 50
-    
+    private let width = UIScreen.main.bounds.width - 50
+    private let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+
     init(text: String) {
         super.init(frame: .zero)
-        placeholder = "   " + text
+        placeholder = text
         backgroundColor = .backgroundSecondary
         clipsToBounds = true
         layer.cornerRadius = 10
@@ -22,6 +23,15 @@ final class CustomTextField: UITextField {
     
     override var intrinsicContentSize: CGSize {
         return CGSize(width: width, height: 44)
+    }
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
     
     required init?(coder: NSCoder) {
